f168a44527b4e7bee03210e233d8199dedbc7759
Wanusa Pontes <wanusapontesest@gmail.com>
2020-04-29 20:28:18 -0300

    update

--- models/sdsa.py
+++ models/sdsa.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.model_selection import StratifiedKFold
 
-class Modelo:
+class SDSA:
 
     def __init__ (self, k):
         self.k = k
@@ -33,7 +33,9 @@ class Modelo:
 
         for i in range(100):
 
-            distance = cdist(X, medias)
+            d_min = cdist(X[:,::2], medias)
+            d_max = cdist(X[:,1::2], medias)    
+            distance = d_min + d_max
             C  = np.argmin(distance, axis=1)
 
             #print(distance)
@@ -45,11 +47,11 @@ class Modelo:
             for c in range(self.k): 
                 medias.append(np.mean(X[(C == c)], axis = 0))
 
-        d_min = cdist(X.iloc[:,0:len(X.columns)-1:2], medias)
-        d_max = cdist(X.iloc[:,1:len(X.columns)-1:2], medias)
+        d_min = cdist(X[:,::2], medias)
+        d_max = cdist(X[:,1::2], medias)
 
 
-        D = sum(d_min, d_max)
+        D = d_min + d_max
 
         clf = DecisionTreeClassifier(max_depth=3)
         clf.fit(D,Y)
@@ -57,50 +59,15 @@ class Modelo:
         self.medias = medias
         return self
 
-    def acuracia(self, X, Y):
+    def accuracy(self, X, Y):
         #teste e retorna acuracia
         #predições para o teste
         D = cdist(X, self.medias)
         predicoes = self.clf.predict(D)
         
-        acuracia = np.sum(predicoes == Y)/len(predicoes)
-        return acuracia    
+        accuracy = np.sum(predicoes == Y)/len(predicoes)
+        return accuracy    
 
-class Simulacao:
-    def __init__ (self, n):
-        self.n = n
-    #funcao de montecarlo 
-    def montecarlo(self):
-        np.random.seed(42)
-        resultados = []
-        for _ in range(self.n): 
-            #np.random.seed(mc)
-            indices_emb = np.random.choice(150, 150, replace = False)
-            dados_embaralhados = iris['data'][indices_emb]
-            target_embaralhados = iris['target'][indices_emb]
-            skf = StratifiedKFold(n_splits=5) #dividindo em 5 grupos para ter como treino e teste
-            #print(skf)
-            #validação cruzada
-            for train_index, test_index in skf.split(dados_embaralhados, target_embaralhados):
-                treino = {
-                    'data': dados_embaralhados[train_index],
-                    'target': target_embaralhados[train_index]
-                }
-                teste = {
-                    'data': dados_embaralhados[test_index],
-                    'target': target_embaralhados[test_index]
-                }
-
-                n = Modelo(k = 4)
-                n.fit(X = treino['data'], Y = treino['target'])
-                acc = n.acuracia( X = teste['data'], Y = teste['target'])
-                resultados.append(acc)
-
-        return resultados
-
-
-print(Simulacao(n = 5).montecarlo())
-#print(type(iris['data']))
 
 
 
