9c2467b1e689f647169f793423b0406c1969b0f6
Wanusa Pontes <wanusapontesest@gmail.com>
2023-09-19 00:50:53 -0300

    add synthetic data

---
+++ main_synthetic_datasets.py
@@ -0,0 +1,313 @@
+from __future__ import division
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+from util.functions import generate_multivariate_gaussians
+from models.sdsa import SDSA
+import argparse
+import time
+import os
+
+# Parallelization
+import itertools
+from multiprocessing import cpu_count, Pool
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
+classifiers = {
+      'sdsa' : SDSA,
+      'sdsa_not_update' : SDSA,
+      'sdsa_rf' : SDSA,
+      'sdsa_rf_not_update' : SDSA,
+      'sdsa_svc' : SDSA,
+      'sdsa_svc_not_update' : SDSA,
+      'sdsa_lr' : SDSA,
+      'sdsa_lr_not_update' : SDSA
+}
+
+parameters_synthetic = {
+    'sdsa_rf': {'dataset1': {"gaussians": np.array([
+                [99, 9, 99, 169, 200, 0],
+                [108, 9, 99, 169, 200, 1]
+            ])},
+            'dataset2':   {"gaussians": np.array([
+                [99, 9, 99, 169, 200, 0],
+                [104, 16, 138, 16, 150, 0],
+                [104, 16, 60, 16, 150, 1],
+                [108, 9, 99, 169, 200, 1]
+            ])},
+            'dataset3': {"gaussians": np.array([
+                        [99, 9, 99, 169, 44, 25, 200, 0],
+                        [104, 16, 138, 16, 44, 25, 150, 0],
+                        [104, 16, 60, 16, 41, 25, 150, 1],
+                        [108, 9, 99, 169, 41, 25, 200, 1]
+            ])},
+            'dataset4': { "gaussians": np.array([
+                        [99, 9, 99, 169, 200, 0],
+                        [104, 16, 118, 16, 150, 0],
+                        [104, 16, 80, 16, 150, 1],
+                        [100, 9, 99, 169, 200, 1]
+                    ])}
+}}
+
+parameters = {
+    'sdsa_rf': {
+        'dataset1': {'k': [4, 2], 'update': True, 
+                    'classifier': RandomForestClassifier, 
+                    'parameters' : {}
+        },
+        'dataset2': {'k': [28, 38], 'update': True, 
+                    'classifier': RandomForestClassifier, 
+                    'parameters' : {}
+          },
+        'dataset3': {'k': [28, 38], 'update': True, 
+                    'classifier': RandomForestClassifier, 
+                    'parameters' : {}
+                    },
+        'dataset4': {'k': [20, 40], 'update': True,  
+                    'classifier': RandomForestClassifier, 
+                    'parameters' : {}
+                   }
+    }
+}
+
+
+# mc_iterations = 1
+# n_folds = 10
+
+
+# if __name__ == '__main__':
+#     np.random.seed(1)
+#     #for dataset_name in parameters['sdsa_rf']:
+#     dataset_name = 'dataset1'
+#     params = parameters['sdsa_rf'][dataset_name]
+#     dataset =  generate_multivariate_gaussians(params["gaussians"], 10)
+#     # df_pd = pd.DataFrame(dataset.data)
+#     # df_pd['target'] = dataset.target
+#     # print(dataset.data, dataset.target)
+#     X, y = dataset.data, dataset.target
+#     skf = StratifiedShuffleSplit(test_size=0.5)
+#     for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+#         if i == 0:
+#             X_train, y_train = X[train_index], y[train_index]
+#             X_test, y_test = X[test_index], y[test_index]
+#             sdsa = SDSA(params["k"], params["update"], params["classifier"],params['parameters'])
+#             sdsa.fit(X_train, y_train)
+
+#         # if dataset_name == "dataset3":
+#         #     df = pd.DataFrame(data=X,columns=["x1_min","x1_max","x2_min","x2_max","x3_min","x3_max"])
+#         #     df['target'] = y
+#         #     df.to_csv("synthetic_datasets\synthetic-{}.csv".format(dataset_name), index=False)
+#         # else:
+#         #     df = pd.DataFrame(data=X,columns=["x1_min","x1_max","x2_min","x2_max"])
+#         #     df['target'] = y
+#         #     # print(df)
+#         #     df.to_csv("synthetic_datasets\synthetic-{}.csv".format(dataset_name), index=False)
+
+columns = ['dataset', 'n_classes', 'n_features', 'n_samples', 'classifier', 'mc',
+           'test_fold', 'acc', 'exec_time']
+
+
+def comma_separated_strings(s):
+    try:
+        return s.split(',')
+    except ValueError:
+        msg = "Not a valid comma separated list: {}".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='''Runs all the experiments
+                                     with the given arguments''',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--classifiers', dest='classifier_names',
+                        type=comma_separated_strings,
+                        default=['sdsa', 'sdsa_not_update', 'sdsa_rf', 'sdsa_rf_not_update',
+                         'sdsa_svc','sdsa_svc_not_update','sdsa_lr', 'sdsa_lr_not_update'],
+                        help='''Classifiers to use for evaluation in a comma
+                        separated list of strings. From the following
+                        options: ''' + ', '.join(classifiers.keys()))
+    parser.add_argument('-i', '--iterations', dest='mc_iterations', type=int,
+                        default=10,
+                        help='Number of Monte Carlo iterations')
+    parser.add_argument('-f', '--folds', dest='n_folds', type=int,
+                        default=10,
+                        help='Folds to create for cross-validation')
+    parser.add_argument('-o', '--output-path', dest='results_path', type=str,
+                        default='results_test',
+                        help='''Path to store all the results''')
+    parser.add_argument('-d', '--datasets', dest='datasets',
+                        type=comma_separated_strings,
+                        default=['climates', 'dry-climates', 'european-climates', 'mushroom'],
+                        help='''Comma separated dataset names''')
+    parser.add_argument('-w', '--workers', dest='n_workers', type=int,
+                        default=-1,
+                        help='''Number of jobs to run concurrently. -1 to use all
+                                available CPUs''')
+    return parser.parse_args()
+
+
+def compute_all(args):
+    ''' Train a classifier with the specified dataset
+
+    Parameters
+    ----------
+    args is a tuple with all the following:
+
+    dataset : string
+        Name of the dataset to use
+    n_folds : int
+        Number of folds to perform n-fold-cross-validation to train and test
+        the classifier.
+    mc : int
+        Monte Carlo repetition index, in order to set different seeds to
+        different repetitions, but same seed in calibrators in the same Monte
+        Carlo repetition.
+    classifier_name : string
+        Name of the classifier to be trained and tested
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        DataFrame with the overall results of every calibration method
+        dataset : string
+            Name of the dataset
+        classifier : string
+            Classifier name
+        n_classes : int
+            Number of classes in the dataset
+        n_features : int
+            Number of features in the dataset
+        n_samples : int
+            Number of samples in the dataset
+        mc : int
+            Monte Carlo repetition index
+        acc : float
+            Mean accuracy
+        exec_time : float
+            Mean execution time
+    '''
+    (dataset, n_folds, mc, classifier_name, results_path) = args
+
+    classifier = classifiers[classifier_name]
+    params = parameters[classifier_name][dataset]
+
+    
+    data =  generate_multivariate_gaussians(parameters_synthetic[classifier_name][dataset]["gaussians"], 10)
+    X, y = data.data, data.target
+
+    if dataset == 'dataset3':
+        df = pd.DataFrame(data=X,columns=["x1_min","x1_max","x2_min","x2_max","x3_min","x3_max"])
+        df['target'] = y
+    else:
+        df = pd.DataFrame(data=X,columns=["x1_min","x1_max","x2_min","x2_max"])
+        df['target'] = y
+    df.to_csv("synthetic_datasets\synthetic-{}.csv".format(dataset), index=False)
+
+    
+    data = pd.read_csv('./synthetic_datasets/synthetic-{}.csv'.format(dataset)) 
+
+    params = parameters['sdsa_rf'][dataset]
+    # data =  generate_multivariate_gaussians(parameters_synthetic[classifier_name][dataset]["gaussians"], 10)
+
+    # X, y = data.data, data.target
+
+
+    X = data.drop('target', axis=1).values                                                                                                                                         
+
+    y = data['target'].values
+
+    skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=mc)
+    n_classes = len(np.unique(y))
+    
+    results = []
+
+    fold_id = 0
+    for train_idx, test_idx in skf.split(X, y):
+        print(
+            'Computing: classifier: {}, dataset: {}, mc: {} fold: {}'.format(
+                classifier_name, dataset, mc, fold_id
+            )
+        )
+        x_train, y_train = X[train_idx], y[train_idx]
+        x_test, y_test = X[test_idx], y[test_idx]
+
+        c = classifier(**params)
+        # c = classifier(params["k"], params["update"], params["classifier"],params['parameters'])
+        start = time.time()
+        c.fit(x_train, y_train)
+        end = time.time()
+
+        exec_time = end - start
+
+        acc = c.accuracy(x_test, y_test)
+        results.append(
+            [dataset, n_classes, X.shape[1]/2, X.shape[0], classifier_name, mc,
+           fold_id, acc, exec_time]
+        )
+
+        fold_id += 1
+    df = pd.DataFrame(data=results, columns=columns)
+    df.to_csv(os.path.join(results_path, 'dataset-{}-mc-{}.csv'.format(dataset, mc)))
+    return df
+
+
+def main(mc_iterations, n_folds, classifier_names, results_path,
+		 datasets, n_workers):
+
+    dataset_names = datasets
+    dataset_names.sort()
+
+    classifier_names.sort()
+    results_path_root = results_path
+
+    for classifier_name in classifier_names:
+        all_results = []
+        results_path = os.path.join(results_path_root, classifier_name)
+
+        if not os.path.exists(results_path):
+            os.makedirs(results_path)
+        
+        for dataset in dataset_names:
+            mcs = np.arange(mc_iterations)
+
+            args = [[dataset], [n_folds], mcs, [classifier_name], [results_path]]
+            args = list(itertools.product(*args))
+
+            if n_workers == -1:
+                n_workers = cpu_count()
+
+            if n_workers == 1:
+                map_f = map
+            else:
+                if n_workers > len(args):
+                    n_workers = len(args)
+
+                p = Pool(n_workers)
+                map_f = p.map
+
+            dfs = map_f(compute_all, args)
+            all_results.extend(dfs)
+        all_results = pd.concat(all_results)
+        all_results.to_csv(
+            os.path.join(
+                results_path, '{}.csv'.format(','.join(dataset_names))
+            )
+        )
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main(**vars(args))
+
+    
+
---
+++ util/functions.py
@@ -0,0 +1,81 @@
+from __future__ import division
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+
+class Dataset:
+    def __init__(self, dataframe=None, data=None, target=None):
+        if dataframe is not None:
+            data_columns = dataframe.columns[dataframe.columns != "label"]
+            self.data = dataframe[data_columns].values
+            self.target = dataframe["label"].values
+        else:
+            self.data = data
+            self.target = target
+
+
+def get_dataset(filename):
+    data = pd.read_csv(filename,  header=0)
+    return Dataset(dataframe=data)
+
+
+def generate_multivariate_gaussians(parameters, interval):
+    quantities = parameters[:, -2]
+    n = np.sum(quantities)
+    p = parameters[:, :-2].shape[1]
+    labels = parameters[:, -1]
+    data = np.zeros((n, p))
+    target = np.zeros(n).astype(int)
+    n_gaussians = parameters.shape[0]
+
+    end = 0
+    params = parameters[:, :-2]
+    for gaussian in np.arange(n_gaussians):
+        init = end
+        end += quantities[gaussian]
+        means = params[gaussian, ::2]
+        covs = np.diag(params[gaussian, 1::2].astype(float))
+        n_g = quantities[gaussian]
+        d = np.random.multivariate_normal(means, covs, n_g)
+
+        delta = np.random.randint(1, interval, int(p / 2))
+        data[init:end, ::2] = d - (delta / 2.0)
+        data[init:end, 1::2] = d + (delta / 2.0)
+        target[init:end] = labels[gaussian]
+    return Dataset(data=data, target=target)
+
+
+def print_confusion_matrix(y_true, y_pred, classes=None, title="",
+                           caption=""):
+    cm = confusion_matrix(y_true, y_pred)
+    labels = np.unique(y_true)
+    if classes is None:
+        classes = labels + 1
+    print("\\begin{table}[H]")
+    print("    \\centering")
+    print("    \\caption{\\textbf{" + title + "} - " + caption + "}")
+    s = "    \\begin{tabular}{c"
+    for label in labels:
+        s += "|c"
+    print(s + "|c}")
+    s = "    \\toprule "
+    for c in classes:
+        s += "& {} ".format(c)
+    print(s + " & Total \\\\")
+    print("    \\midrule")
+    p = "Predicoes "
+    for i, l1 in enumerate(labels):
+        s = "{} ".format(classes[l1])
+        for l2 in labels:
+            s += "& {} ".format(cm[l1, l2])
+        s += "& {} \\\\".format(np.sum(cm[l1]))
+        print(s + " \\midrule")
+        p += "& {} ".format(np.sum(cm[:, l1]))
+    p += "& {} \\\\".format(np.sum(cm))
+    print(p)
+    print("    \\bottomrule")
+    print("    \\end{tabular}")
+    print("    \\label{tab:tab}")
+    print("\\end{table}")
